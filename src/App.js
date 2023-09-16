e01aff1626707bf553fed4fbc17d34d57aba23fa
speer987 <saarah.peer15@gmail.com>
2023-09-16 12:33:02 -0700

    adding html and css for front page (sign in)

--- src/App.js
+++ src/App.js
@@ -4,8 +4,22 @@ import image from './images/sign-up-page.jpg'
 function App() {
   return (
     <div className="App">
-      <img src={image} alt="picture of a floating book" height='100px'></img>
-      <div>This is my app</div>
+      <div class="float-container">
+        <header>The Book Nook</header>
+          <div class='flex-box'>
+            <img class="float-child flex-child" src={image} alt="floating book" height='100px'></img>
+            <div class="float-child sign-up">
+            <form className="flex-form">
+              <h1 className="centered-text">Sign In</h1>
+              <label for="email">Email</label>
+              <input type="text" id="email" name="fname"/>
+              <label for="password">Password</label>
+              <input type="password" id="password" name="fname"/>
+              <p id="new-user-text">New user? Register here.</p>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
