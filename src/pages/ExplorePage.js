d1be5e94805cbf61a5e61f27c60710a83ad388a7
speer987 <saarah.peer15@gmail.com>
2023-09-30 11:41:43 -0700

    adding updates to get categories of books from NYT api

--- src/pages/ExplorePage.js
+++ src/pages/ExplorePage.js
@@ -1,14 +1,61 @@
 import Header from "../components/Header";
 // import { ReadingLogPage } from "./ReadingLogPage";
 import OptionsMenu from "../components/OptionsMenu";
+import { useState, useEffect } from "react";
 
 const ExplorePage = () => {
+  const [currentBest, setCurrentBest] = useState("");
+  const [categoryList, setCategoryList] = useState("");
+  const apiKey = `LrgMgEJkKjWGZ5zXLBAd3voRBfD2s6aD`;
+  let url = "";
+
+  useEffect(() => {
+    url =
+      `https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=` + apiKey;
+    console.log(url);
+    fetch(url)
+      .then((response) => response.json())
+      .then((response) => setCategoryList(response))
+      .catch((error) => setCategoryList(error));
+  }, []);
+
+  const categoryInfo = categoryList?.results;
+  const categories = categoryInfo?.map((category) => category.list_name);
+  console.log(categories);
+
+  const urlList = categories?.map(
+    (category) =>
+      `https://api.nytimes.com/svc/books/v3/lists/current/` +
+      category +
+      `.json?api-key=` +
+      apiKey
+  );
+  console.log(urlList);
+
+  // const bookCategoryDataList = urlList.map();
+  // useEffect(() => {
+  //   const url =
+  //     `https://api.nytimes.com/svc/books/v3/lists/current/paperback-nonfiction.json?api-key=` +
+  //     apiKey;
+  //   console.log(url);
+  //   fetch(url)
+  //     .then((response) => response.json())
+  //     .then((response) => setCurrentBest(response))
+  //     .catch((error) => setCurrentBest(error));
+  // }, []);
+
+  // console.log(currentBest);
+
   return (
     <div>
       <Header />
       <OptionsMenu selected="explore" />
       <h1 class="page-title">Explore</h1>
-      <div>Best Selling Books</div>
+      <div class="subtitle">New York Time's Best Selling Books This Week</div>
+      <div>Add a dropdown here</div>
+
+      <div>Search</div>
+      <div></div>
     </div>
   );
 };
