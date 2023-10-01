71b294349da5c54e1861fdc583c43e2165c7fc77
speer987 <saarah.peer15@gmail.com>
2023-09-30 19:13:02 -0700

    adding book cover images

--- src/pages/ExplorePage.js
+++ src/pages/ExplorePage.js
@@ -10,29 +10,31 @@ const ExplorePage = () => {
   const apiKey = `LrgMgEJkKjWGZ5zXLBAd3voRBfD2s6aD`;
   let url = "";
 
-  useEffect(() => {
-    url =
-      `https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=` + apiKey;
-    console.log(url);
-    fetch(url)
-      .then((response) => response.json())
-      .then((response) => setCategoryList(response))
-      .catch((error) => setCategoryList(error));
-  }, []);
+  let dummyList = ["Hardcover Fiction"];
 
-  const categoryInfo = categoryList?.results;
-  const categories = categoryInfo
-    ?.slice(0, 10)
-    ?.map((category) => category.list_name);
-  console.log(categories);
+  // useEffect(() => {
+  //   url =
+  //     `https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=` + apiKey;
+  //   console.log(url);
+  //   fetch(url)
+  //     .then((response) => response.json())
+  //     .then((response) => setCategoryList(response))
+  //     .catch((error) => setCategoryList(error));
+  // }, []);
 
-  const urlList = categories?.map(
-    (category) =>
-      `https://api.nytimes.com/svc/books/v3/lists/current/` +
-      category +
-      `.json?api-key=` +
-      apiKey
-  );
+  // const categoryInfo = categoryList?.results;
+  // const categories = categoryInfo
+  //   ?.slice(0, 10)
+  //   ?.map((category) => category.list_name);
+  // console.log(categories);
+
+  // const urlList = categories?.map(
+  //   (category) =>
+  //     `https://api.nytimes.com/svc/books/v3/lists/current/` +
+  //     category +
+  //     `.json?api-key=` +
+  //     apiKey
+  // );
   // console.log(urlList?.[0]);
 
   // useEffect(() => {
@@ -60,6 +62,19 @@ const ExplorePage = () => {
 
   // console.log(currentBest);
 
+  useEffect(() => {
+    const url =
+      `https://api.nytimes.com/svc/books/v3/lists/current/${dummyList}.json?api-key=` +
+      apiKey;
+    console.log(url);
+    fetch(url)
+      .then((response) => response.json())
+      .then((response) => setCurrentBest(response))
+      .catch((error) => setCurrentBest(error));
+  }, []);
+  const bookList = currentBest?.results?.books;
+  // console.log(currentBest?.results?.books?.[0]?.book_image);
+
   return (
     <div>
       <Header />
@@ -67,8 +82,13 @@ const ExplorePage = () => {
       <h1 class="page-title">Explore</h1>
       <div class="subtitle">New York Time's Best Selling Books This Week</div>
       {/* <label for="genre">Genre:</label> */}
-      <Dropdown list={categories} purpose="Genre" />
+      {/* <Dropdown list={categories} purpose="Genre" /> */}
+      <Dropdown list={dummyList} purpose="Genre" />
       <div class="flex-container flex-row covers-container">
+        {bookList?.map((bookInfo) => (
+          <img src={bookInfo?.book_image} />
+        ))}
+        {/* console.log(bookInfo?.book_image) */}
         <div class="book-cover">box</div>
         <div class="book-cover">box</div>
         <div class="book-cover">box</div>
