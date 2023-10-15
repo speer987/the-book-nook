2489b6e2d9445c9c93d25c42226caeb87bf67ae6
speer987 <saarah.peer15@gmail.com>
2023-10-15 15:48:50 -0700

    adding hover states to book covers using tailwind css

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,9 +1,9 @@
 export default function BookList({ bookList }) {
   return (
-    <div class="flex-container flex-row covers-container">
+    <div class="flex flex-row flex-wrap justify-between mt-6">
       {bookList?.map((bookInfo) => (
         <img
-          class="book-cover"
+          class="w-1/6 rounded-lg m-5 hover:scale-110 transition ease-in-out hover:shadow-lg shadow-teal-900"
           key={bookInfo?.book_image}
           src={bookInfo?.book_image}
         />
--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,6 +1,6 @@
 import BookBlurb from "../components/BookBlurb";
 import Header from "../components/Header";
-import OptionsMenu from "../components/OptionsMenu";
+import CurrentPageWithInfo from "../components/CurrentPageWithInfo";
 import { useState } from "react";
 import TextInput from "../components/TextInput";
 import SubmitButton from "../components/SubmitButton";
@@ -11,11 +11,13 @@ const SearchPage = () => {
 
   return (
     <div>
-      <div>
-        <Header loggedIn={true} />
+      <Header loggedIn={true} />
+      <div class="bg-gradient-to-b from-teal-100 text-lg p-7">
+        <CurrentPageWithInfo
+          page="Search"
+          info={"Books by Title and/or Author"}
+        />
       </div>
-      <h1 className="page-title">Search</h1>
-      <div className="subtitle">Books by Title and/or Author</div>
       <TextInput
         displayName="Title"
         type="text"
