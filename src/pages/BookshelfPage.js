3c8591ae33ed82e30ef675c24b5455b63bf5434f
speer987 <saarah.peer15@gmail.com>
2023-11-15 12:02:37 -0800

    adding changes to input

--- src/pages/BookshelfPage.js
+++ src/pages/BookshelfPage.js
@@ -16,14 +16,18 @@ const BookshelfPage = () => {
           <div class="border-solid border-2 border-blue-200 m-5 text-lg font-title text-teal-900 font-bold">
             Book Progress
           </div>
+          <div class="border-solid border-2 border-blue-200 m-5 text-lg font-title text-teal-900 font-bold">
+            Book Title Goes Here
+          </div>
           <div class="border-solid border-2 border-blue-200 m-5 text-lg font-title text-teal-900">
             Graph goes here
           </div>
           <div class="flex border-solid border-2 border-blue-200 m-5">
-            <div class="flex border-solid border-2 border-blue-200 w-2/3">
-              Log Textbar
-            </div>
-            <button class="flex border-solid border-2 border-blue-200 w-1/3">
+            <input
+              type="number"
+              class="rounded flex border-solid border-2 border-slate-300 w-2/3 font-body h-8"
+            ></input>
+            <button class="rounded bg-teal-900 m-1.5 border-solid w-1/3 h-8 text-slate-100 text-center">
               Log
             </button>
           </div>
