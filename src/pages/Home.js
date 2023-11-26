149783601d0fdd8b39a05fb0a89dbbf614e9cc0e
speer987 <saarah.peer15@gmail.com>
2023-11-26 13:00:53 -0800

    adding updated home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,30 +3,36 @@ import Header from "../components/Header";
 import google_logo from "../images/google.png";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { SignIn } from "../services/AuthServices";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   const navigate = useNavigate();
   return (
-    // <div>Landing Page</div>
     <div className="bg-home w-screen h-screen bg-cover flex flex-col items-center justify-center">
       <div className="w-1/3 h-1/5 bg-teal-900 rounded-t-xl flex flex-col items-center justify-center text-slate-50">
         <div className="font-title text-3xl font-semibold">The Book Nook</div>
         <div className="font-title text-lg">Find Your Next Good Book</div>
       </div>
       <div className="w-1/3 h-2/5 bg-white border-2 border-teal-900 rounded-b-xl flex flex-col items-center justify-center">
-        <div className="font-body text-lg">Sign In With Google</div>
-        <button onClick={() => navigate("/explore")}>
-          <img
-            className="w-24 h-24 m-5 p-5 rounded bg-slate-50 hover:bg-teal-100 transition ease-in-out"
-            src={google_logo}
-          ></img>
-        </button>
-        <a
-          href="/explore"
-          className="font-body rounded text hover:text-teal-800 transition ease-in-out"
+        <Link
+          class="bg-teal-200 active:bg-teal-100 focus:text-teal-900 rounded-lg hover:bg-teal-800 hover:text-slate-50 transition duration-500 ease-in-out font-body p-4"
+          to={"../explore"}
         >
-          or Continue As A Guest
-        </a>
+          Explore Trending Books
+        </Link>
+        <Link
+          class="bg-teal-200 active:bg-teal-100 focus:text-teal-900 rounded-lg hover:bg-teal-800 hover:text-slate-50 transition duration-500 ease-in-out font-body p-4"
+          to={"../search"}
+        >
+          Search for A Book
+        </Link>
+        <Link
+          class="bg-teal-200 active:bg-teal-100 focus:text-teal-900 rounded-lg hover:bg-teal-800 hover:text-slate-50 transition duration-500 ease-in-out font-body p-4"
+          to={"../bookshelf"}
+        >
+          Maintain Your Virtual Bookshelf
+        </Link>
       </div>
     </div>
   );
